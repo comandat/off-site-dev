6d39e6d005a107a0e3682ac193b68276a47b0636
comandat <contact@comandat.ro>
2025-10-27 16:54:59 +0200

    Create state.js

---
+++ scripts/state.js
@@ -0,0 +1,16 @@
+// scripts/state.js
+
+export const state = {
+    currentCommandId: null,
+    currentManifestSKU: null,
+    currentProductId: null,
+    editedProductData: {},
+    activeVersionKey: 'origin',
+    descriptionEditorMode: 'raw',
+    sortableInstance: null,
+    competitionDataCache: null,
+    productScrollPosition: 0,
+    currentSearchQuery: '',
+    currentView: 'comenzi',
+    searchTimeout: null
+};
